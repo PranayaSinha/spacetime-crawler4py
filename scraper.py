07735992ccabe26401b234d1b10e3bde2856d999
PranayaSinha <PranayaSinha@gmail.com>
2023-02-10 18:51:05 -0800

    Fix Is Valid

--- scraper.py
+++ scraper.py
@@ -24,13 +24,17 @@ def extract_next_links(url, resp):
     soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
     for link in soup.find_all('a'):
         link = link.get('href')
-        if link:
+        # Detect and avoid dead URLs that return a 200 status but no data
+        if (link and resp.status == 200):
             # Defragment the URL
             link = urlparse(link)._replace(fragment='').geturl()
             # Transform relative to absolute URL
             link = urljoin(url, link)
             links.append(link)
             print(link)
+    with open("Res.txt", "a") as file:
+        file.write(str(soup))
+
     return links
 
 def is_valid(url):
@@ -41,19 +45,12 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        # Check if the URL is within the specified domains and paths
-        if not re.match(r".*\.(ics|cs|informatics|stat)\.uci\.edu/.*", parsed.netloc):
-            return False
+        
         # Detect and avoid large files, especially if they have low information value
-
-        # Detect and avoid dead URLs that return a 200 status but no data
 
         # Detect and avoid sets of similar pages with no information
 
         # Detect and avoid infinite traps
-        
-        # Honor the politeness delay for each site
-        time.sleep(1)
 
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
